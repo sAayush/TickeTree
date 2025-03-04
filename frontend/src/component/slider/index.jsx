1313d0a677a70e4a6e441601ac78c7aff96a5c7a
Bhinsar <bhinsarjagdish2003@gmail.com>
2025-03-04 20:43:36 +0530

    add icons in input make them responsive add slider at home page

---
+++ frontend/src/component/slider/index.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import { FaRegCaretSquareLeft, FaRegCaretSquareRight } from "react-icons/fa";
+import "./style.scss";
+
+function Slider({ slider }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [fade, setFade] = useState(false);
+
+  const goToPrevious = () => {
+    setFade(true);
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex > 0 ? prevIndex - 1 : slider.length - 1
+      );
+      setFade(false);
+    }, 500); // Match this with the CSS transition duration
+  };
+
+  const goToNext = () => {
+    setFade(true);
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex < slider.length - 1 ? prevIndex + 1 : 0
+      );
+      setFade(false);
+    }, 500); // Match this with the CSS transition duration
+  };
+
+  // Auto slide effect
+  useEffect(() => {
+    const interval = setInterval(() => {
+      goToNext();
+    }, 3000); // Change slide every 3 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
+  return (
+    <div className="slider-slide">
+      <div
+        className={`slider-wrapper ${fade ? "fade" : ""}`}
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {slider.map((slide, index) => (
+          <div
+            key={index}
+            className="slider-container"
+            style={{ backgroundImage: `url(${slide.image})` }}
+          >
+            <div className="slider-content">
+              <div className="slider-content-bottom">
+                <h1>{slide.title}</h1>
+                <p>{slide.description}</p>
+              </div>
+              <div className="slider-content-top">
+                <FaRegCaretSquareLeft
+                  className="slider-content-top-left"
+                  onClick={goToPrevious}
+                />
+                <FaRegCaretSquareRight
+                  className="slider-content-top-right"
+                  onClick={goToNext}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Slider;
