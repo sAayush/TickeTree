240a98041c13d6509fcac86ccc6a5f6c0c45cfa1
Bhinsar <bhinsarjagdish2003@gmail.com>
2025-03-16 19:36:35 +0530

    feat: update Navbar component with logo and logout functionality; add sass-embedded dependency

--- host-frontend/src/components/nav/index.jsx
+++ host-frontend/src/components/nav/index.jsx
@@ -1,5 +1,19 @@
-export default function Navbar() {
+import { NavLink } from "react-router-dom";
+import logo from "/logo.png";
+import { IoLogOut } from "react-icons/io5";
+import './style.scss'
+
+export default function Navbar({ onLogout }) {
     return (
-        <div>Nav</div>
+        <div className="nav-component">
+            <NavLink to="/" className="logo">
+                <img src={logo} alt="TicketTree Logo" />
+                <span>Tickettree</span>
+            </NavLink>
+            <div className="logout-button" onClick={onLogout}>
+                <IoLogOut className="nav-icon" />
+                <span>Logout</span>
+            </div>
+        </div>
     );
 }
